b3e8cf4fbbe5a6c006876c3f4cac7c16d41ffa83
dalmatiandots <cenan.ivana@gmail.com>
2023-05-11 12:50:49 +0200

    Update BobRoss.py
    Update HappyBobRoss test branch: update BobRoss,py

--- BobRoss.py
+++ BobRoss.py
@@ -1,3 +1,5 @@
 print("Hello everybody")
 print("Let's talk about painting")
 print("Top: Find freedom on this canvas. Any time you learn, you gain. Believe that you can do it cause you can do it.")
+print("Ajmoo! Ajmoo!")
+print("Idemooo :)")
