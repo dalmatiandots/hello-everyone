44dc2cd5737f2020a05d39945629eb8b205d005b
Cenan <cenan.ivana@gmail.com>
2023-05-11 12:28:46 +0200

    Add a new special Bob Ross file to local project

---
+++ BobRoss.py
@@ -0,0 +1,3 @@
+print("Hello everybody")
+print("Let's talk about painting")
+print("Top: Find freedom on this canvas. Any time you learn, you gain. Believe that you can do it cause you can do it.")
